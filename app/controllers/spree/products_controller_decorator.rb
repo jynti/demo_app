e559b4e64b7ed37b941422f93b542310d2087452
jayanti <jayanti@vinsol.com>
2018-05-02 13:09:57 +0530

    fix code

---
+++ app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,8 @@
+Spree::ProductsController.class_eval do
+    def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @products = @searcher.retrieve_products.published
+      @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+    end
+end
